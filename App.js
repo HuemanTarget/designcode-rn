381c08ecc2ae6891404f208aa3358a44e1c9b226
Joshua Basche <joshbasche@live.com>
2021-09-04 13:00:26 -0700

    create Icons in components

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, SafeAreaView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import styled from 'styled-components';
 import { Card } from './components/Card';
+import { NotificationIcon } from './components/Icons';
 
 export default function App() {
   return (
     <Container>
-      <TitleBar>
-        <Avatar source={require("./assets/avatar.jpg")} />
-        <Title>Welcome back,</Title>
-        <Name>Josh</Name>
-      </TitleBar>
-      <Subtitle>Continue Learning</Subtitle>
-      <Card />
+      <SafeAreaView>
+        <ScrollView>
+          <TitleBar>
+            <Avatar source={require('./assets/avatar.jpg')} />
+            <Title>Welcome back,</Title>
+            <Name>Josh</Name>
+            <NotificationIcon
+              style={{ position: 'absolute', right: 20, top: 5 }}
+            />
+          </TitleBar>
+          <Subtitle>Continue Learning</Subtitle>
+          <ScrollView
+            horizontal={true}
+            style={{ paddingBottom: 30 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Card
+              title='Styled Components 1'
+              image={require('./assets/background2.jpg')}
+              logo={require('./assets/logo-react.png')}
+              subtitle='5 of 12 sections'
+              caption='React Native'
+            />
+            <Card
+              title='Styled Components 2'
+              image={require('./assets/background2.jpg')}
+              logo={require('./assets/logo-react.png')}
+              subtitle='5 of 12 sections'
+              caption='React Native'
+            />
+            <Card
+              title='Styled Components 3'
+              image={require('./assets/background2.jpg')}
+              logo={require('./assets/logo-react.png')}
+              subtitle='5 of 12 sections'
+              caption='React Native'
+            />
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
