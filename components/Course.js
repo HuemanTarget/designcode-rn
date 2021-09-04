9fa129271251b096ecd2c6ec1143c48efdc10132
Joshua Basche <joshbasche@live.com>
2021-09-04 15:05:34 -0700

    add Course component to App and map through course array data

--- components/Course.js
+++ components/Course.js
@@ -5,9 +5,89 @@ export const Course = (props) => (
   <Container>
     <Cover>
       <Image source={props.image} />
-      <Title>{props.title}</Title>
+      <Logo source={props.logo} />
+      <CoverWrapper>
+        <Subtitle>{props.subtitle}</Subtitle>
+        <Title>{props.title}</Title>
+      </CoverWrapper>
     </Cover>
+    <Content>
+      <Avatar source={props.avatar} />
+      <Wrapper>
+        <Caption>{props.caption}</Caption>
+        <Author>Taught by {props.author}</Author>
+      </Wrapper>
+    </Content>
   </Container>
 );
 
-const Container = styled.View``;
+const Container = styled.View`
+  background: white;
+  width: 90%;
+  height: 300px;
+  border-radius: 14px;
+  margin-left: 20px;
+  margin-top: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+`;
+const Cover = styled.View`
+  width: 100%;
+  height: 240px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
+`;
+const CoverWrapper = styled.View`
+  position: absolute;
+  bottom: 10;
+  left: 10;
+`;
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+const Logo = styled.Image`
+  height: 55px;
+  width: 55px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+`;
+const Subtitle = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 500;
+`;
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 10px;
+`;
+const Content = styled.View`
+  padding-left: 20px;
+  flex-direction: row;
+  align-items: center;
+  height: 60px;
+`;
+const Wrapper = styled.View`
+  margin-left: 10px;
+`;
+const Avatar = styled.Image`
+  width: 30px;
+  height: 30px;
+  background: black;
+  border-radius: 15px;
+`;
+const Caption = styled.Text`
+  font-size: 15px;
+  font-weight: 600;
+`;
+const Author = styled.Text`
+  font-size: 12px;
+  color: gray;
+  font-weight: 400;
+`;
