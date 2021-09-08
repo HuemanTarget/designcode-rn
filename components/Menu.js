63993b689d7ef5abc693185e9017ed3955dc449f
Joshua Basche <joshbasche@live.com>
2021-09-08 10:13:34 -0700

    add style to Cover in Menu component

--- components/Menu.js
+++ components/Menu.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { Animated } from "react-native";
+import { Animated, TouchableOpacity, Dimensions } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const screenHeight = Dimensions.get("window").height;
 
 export class Menu extends Component {
   state = {
-    top: new Animated.Value(900),
+    top: new Animated.Value(screenHeight),
   };
 
   componentDidMount() {
@@ -13,10 +16,34 @@ export class Menu extends Component {
     }).start();
   }
 
+  toggleMenu = (props) => {
+    Animated.spring(this.state.top, {
+      toValue: screenHeight,
+    }).start();
+  };
+
   render() {
     return (
       <AnimatedContainer style={{ top: this.state.top }}>
-        <Cover />
+        <Cover>
+          <Image source={require("../assets/background2.jpg")} />
+          <Title>Josh Basche</Title>
+          <Subtitle>Mobile Developer at Pompous Cat</Subtitle>
+        </Cover>
+        <TouchableOpacity
+          onPress={this.toggleMenu}
+          style={{
+            position: "absolute",
+            top: 120,
+            left: "50%",
+            marginLeft: -22,
+            zIndex: 1,
+          }}
+        >
+          <CloseView>
+            <Ionicons name="ios-close" size={44} color="#546bfb" />
+          </CloseView>
+        </TouchableOpacity>
         <Content />
       </AnimatedContainer>
     );
@@ -34,8 +61,34 @@ const AnimatedContainer = Animated.createAnimatedComponent(Container);
 const Cover = styled.View`
   height: 142px;
   background: black;
+  justify-content: center;
+  align-items: center;
+`;
+const Image = styled.Image`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: 600;
+`;
+const Subtitle = styled.Text`
+  font-size: 13px;
+  color: rgba(255, 255, 255, 0.5);
+  margin-top: 8px;
 `;
 const Content = styled.View`
-  height: 900px;
+  height: ${screenHeight};
   background: #f0f3f5;
+`;
+const CloseView = styled.View`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background: white;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
 `;
